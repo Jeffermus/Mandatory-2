3d4a9ace61a63b6cb59fcc8f485fc7119caed250
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2021-11-23 12:44:25 +0100

    test commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
-const { createPage } = require("./render/render.js");
+const { createPage} = require("./render/render.js");
 const createuser = createPage("createUser/createUser.js");
-const adminuser = createPage("admin/admin.js");
+// const adminuser = createPage("admin/admin.js");
 
 app.use(express.static("public"));
 
@@ -35,6 +35,8 @@ const frontpagePage = createPage("frontpage/frontpage.html", {
 const CVPage = createPage("CVPage/CVPage.html");
 const projectsPage = createPage("projects/projects.html");
 const contactPage = createPage("contact/contact.html");
+const adminuser = createPage("admin/admin.html");
+
 
 
 
@@ -72,9 +74,34 @@ app.get("/admin", (req, res) => {
   });
 
 
-app.post("/send-email", (res,req) => {
-
-})
+ app.post('/send-email', function (req, res) {
+              let transporter = nodeMailer.createTransport({
+                  host: 'smtp.office365.com', // Office 365 server
+                  port: 587,     // secure SMTP
+                  secure: false,
+                  auth: {
+                      user: "jeff0866@stud.kea.dk", // Insert mail user into a config file using module.exports
+                      pass: "Mj8d4la#" // Insert mail password into a config file using module.exports
+                  },
+                  tls: {
+                      rejectUnauthorized: false
+                  }
+              });
+              let mailOptions = {
+                  from: "jeff0866@stud.kea.dk", // sender address
+                  to: "jeffoech@gmail.com", // reciver
+                  subject: "<h3>Fra</h3>  Jeffrey", // Subject line
+                  html: "<h3>Hello You!</h3>" // plain text bodyy
+              };
+              let dato = new Date()
+              transporter.sendMail(mailOptions, (error, info) => {
+                  if (error) {
+                      return console.log(dato + error);
+                  }
+                  console.log(dato + 'Message %s sent: %s');
+                      res.render('render');
+                    });
+                  });
   
 const PORT = process.env.PORT || 8080;
 
